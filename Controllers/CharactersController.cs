2629d34280df991572d1c98d04a4c1f713c2d6bc
MikkKivisild <159937176+MikkKivisild@users.noreply.github.com>
2024-10-17 12:04:12 +0300

    Initial commit

---
+++ Controllers/CharactersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TetrisTARpe23.Controllers
+{
+    public class CharactersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Character/CharacterIndexViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace TetrisTARpe23.Models.Character
+{
+    
+        public enum CharacterFriendLvL
+        {
+            Stranger, Friend, Good_Friends, Best_Friend, Homie_Level
+        }
+        public enum AttackComments
+        {
+            Double, Tripple, Tetris
+        }
+        public enum CharacterHP
+        {
+            FullHP, AlmoastFullHP, HalfHP, LowHP, VeryLowHP, Dead
+        }
+        public class CharacterIndexModel
+        {
+            public Guid ID { get; set; }
+            public string CharacterName { get; set; }
+            public int CharacterUsed { get; set; }
+            public int CharacterFriendNextLevel { get; set; }
+            public CharacterFriendLvL CharacterFriendLvL { get; set; }
+            public AttackComments AttackComments { get; set; }
+            public CharacterHP CharacterHP { get; set; }
+        }
+    
+}
