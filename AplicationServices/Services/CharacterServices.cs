56c8140d565e67bfdfcb8491aebd1b01eae44078
MikkKivisild <159937176+MikkKivisild@users.noreply.github.com>
2024-10-17 14:16:42 +0300

    Add files via upload

---
+++ AplicationServices/Services/CharacterServices.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tetris.core.Domain;
+using Tetris.Core.ServiceInterface;
+using Tetris.Data;
+
+namespace Tetris.AplicationServices.Services
+{
+    public class CharacterServices : ICharacterServices
+    {
+        private readonly TetrisCharacterContext _context;
+        // private readonly IFileServices _fileServices;
+
+        public CharacterServices(TetrisCharacterContext context /*IFileServices fileServices*/)
+        {
+            _context = context;
+            //_fileServices = fileServices
+        }
+        /// <summary>
+        /// Get Details for one Character
+        /// </summary>
+        /// <param name="id">Id of character to show details of</param>
+        /// <returns>resulting character</returns>
+        public async Task<Character>DetailsAsync(Guid id)
+        {
+            var result = await _context.Characters
+                .FirstOrDefaultAsync(x => x.ID == id);
+            return result;
+        }
+    }
+}
---
+++ Core/Dto/CharacterDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.Core.Dto
+{
+ public enum CharacterFriendLvL
+        {
+            Stranger, Friend, Good_Friends, Best_Friend, Homie_Level
+        }
+        public enum AttackComments
+        {
+            Double, Tripple, Tetris
+        }
+        public enum CharacterHP
+        {
+            FullHP, AlmoastFullHP, HalfHP, LowHP, VeryLowHP, Dead
+        }
+    public class CharacterDto
+    {
+            public Guid ID { get; set; }
+            public string CharacterName { get; set; }
+            public int CharacterUsed { get; set; }
+            public int CharacterFriendNextLevel { get; set; }
+            public CharacterFriendLvL CharacterFriendLvL { get; set; }
+            public AttackComments AttackComments { get; set; }
+            public CharacterHP CharacterHP { get; set; }
+
+        //image 
+        /*
+         public List<IFrimFile> Files { get; set; }
+        public IEnumerable<FileDatabaseDto> Image { get; set; } = new List<FileToDatabaseDto>();
+         */
+    }
+}
